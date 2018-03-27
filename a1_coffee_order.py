ef02866edf70be5ed6c9bb4c4d4e1ca30734e019
freon-lunarion <antonio.daniswara@gmail.com>
2018-03-27 22:18:39 +1100

    add coffee resources

--- a1_coffee_order.py
+++ a1_coffee_order.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, url_for
 from flask_restful import reqparse
 from model import Coffee, Order, Payment
 
@@ -16,124 +16,144 @@ coffee_db = [
     Coffee(5, 'flat white', 3.60),
 ]
 
-
-@app.route("/orders", methods=['POST'])
-def add_order():
-    global order_counter
-    parser = reqparse.RequestParser()
-    parser.add_argument('coffee_type', type=int, required=True)
-    parser.add_argument('additions', type=str, action='append')
-    args = parser.parse_args()
-    order_counter += 1
-
-    coffee_type = args.get("coffee_type")
-    additions = args.get("additions")
-    for row in coffee_db:
-        if int(row.coffee_id) == int(coffee_type):
-            cost = row.cost
-            break
-    else:
-        return jsonify(coffee_type=False), 404
-
-    ord = Order(order_counter, coffee_type, cost, 'open', additions)
-    order_db.append(ord)
-
-    return jsonify({'data': ord.__dict__, 'link': {'payment': ''}}), 201
-
-
 @app.route("/orders", methods=['GET'])
 def get_orders():
-    status = request.args.get('status')
+    return jsonify([st.__dict__ for st in order_db])
 
-    if status is None:
-        return jsonify([st.__dict__ for st in order_db])
-    else:
-        return jsonify([row.__dict__ for row in order_db if str(row.status).lower() == str(status).lower()])
+@app.route('/orders/status/<status>', methods=['GET'])
+def get_order_bystatus(status):
+    return jsonify([row.__dict__ for row in order_db if str(row.status).lower() == str(status).lower()])
 
-
-@app.route('/orders/<int:id>', methods=['GET'])
-def get_order_id(id):
+@app.route('/orders/<order_id>', methods=['GET'])
+def get_order_byid(order_id):
     for row in order_db:
-        if int(row.order_id) == int(id):
-            return jsonify(row.__dict__)
+        if int(row.order_id) == int(order_id):
+            return jsonify([row.__dict__])
     return jsonify(order_id=False), 404
 
-
-@app.route("/orders/<id>", methods=['DELETE'])
-def delete_order(id):
-    # can be deleted before payment
-    for row in order_db:
-
-        if int(row.order_id) == int(id):
-            order_db.remove(row)
-            return jsonify(order_id=id), 204
-    return jsonify(order_id=False), 400
-
-
-@app.route("/orders/<id>", methods=['PUT'])
-def update_order(id):
-    # can be updated if status is 'open' and no payment
+@app.route("/orders", methods=['POST'])
+def create_order():
+    global order_counter
     parser = reqparse.RequestParser()
-    parser.add_argument('coffee_type', type=int, required=True)
+    parser.add_argument('coffee_id', type=int, required=True)
     parser.add_argument('additions', type=str, action='append')
     args = parser.parse_args()
-    coffee_type = args.get("coffee_type")
+
+    coffee_id = args.get("coffee_id")
     additions = args.get("additions")
     for row in coffee_db:
-        if int(row.coffee_id) == int(coffee_type):
-            cost = row.cost
+        if int(row.coffee_id) == int(coffee_id):
+            coffee = row
             break
     else:
-        return jsonify(coffee_type=False), 400
+        return jsonify(coffee_id=False), 404
 
-    is_paid = any(int(row.order_id) == int(id) for row in payment_db)
+    order_counter += 1
+    new = Order(order_counter, coffee_id, coffee.coffee_name, coffee.cost, 'open', additions)
+    order_db.append(new)
+    return jsonify({
+        'data':new.__dict__,
+        'payment': {
+            'link':url_for('create_payment',order_id=order_counter),
+            'method':'POST'
+        }
 
-    if any(row.order_id == int(id) for row in order_db):
-        for row in order_db:
-            if int(row.order_id) == int(id) and row.status == 'open' and not is_paid:
-                order_db.remove(row)
-                new = Order(id, coffee_type, cost, 'open', additions)
-                order_db.append(new)
-                return jsonify([new.__dict__]), 201
+    }), 201
+
+@app.route("/orders/<order_id>", methods=['DELETE'])
+def delete_order(order_id):
+    # can be deleted before payment
+    row = next((x for x in order_db if x.order_id == int(order_id)), None)
+    is_paid = any(int(row.order_id) == int(order_id) for row in payment_db)
+
+    if row is None:
+        return jsonify(order_id=False), 404
+    elif is_paid:
+        return jsonify(is_paid = True), 401
     else:
+        del order_db[order_db.index(row)]
+        return jsonify(order_id=order_id), 204
+
+@app.route("/orders/<order_id>", methods=['PUT'])
+def update_order(order_id):
+    # can be updated if status is 'open' and no payment
+    parser = reqparse.RequestParser()
+    parser.add_argument('coffee_id', type=int, required=True)
+    parser.add_argument('additions', type=str, action='append')
+    old = next((x for x in order_db if x.order_id == int(order_id)), None)
+
+    if old is None:
         return jsonify(order_id=False), 404
 
-    return jsonify(order_id=False), 404
+    if old.status is not 'open':
+        return jsonify(status=False), 401
 
+    is_paid = any(int(row.order_id) == int(order_id) for row in payment_db)
+    if is_paid:
+        return jsonify(is_paid=True), 401
 
-@app.route("/orders/<id>", methods=['PATCH'])
-def update_order_status(id):
+    args = parser.parse_args()
+    coffee_id = args.get("coffee_id")
+    additions = args.get("additions")
+
+    for item in coffee_db:
+        if item.coffee_id == coffee_id:
+            coffee = item
+            break
+    else:
+        return jsonify( coffee_id=False), 404
+
+    old.coffee_id = coffee_id
+    old.coffee_name = coffee.coffee_name
+    old.cost = coffee.cost
+    old.status = 'open'
+    old.additions = additions
+
+    return jsonify({
+        'data': old.__dict__,
+        'payment': {
+            'link':url_for('create_payment',order_id=order_counter),
+            'method':'POST'
+        }
+    }), 201
+
+@app.route("/orders/<order_id>", methods=['PATCH'])
+def update_order_status(order_id):
+    row = next((x for x in order_db if x.order_id == int(order_id)), None)
+    if row is None:
+        return jsonify(order_id=False), 404
+
     parser = reqparse.RequestParser()
-    parser.add_argument('coffee_type', type=int)
+    parser.add_argument('status', type=str, required=True)
     args = parser.parse_args()
     status = args.get("status")
+    is_paid = any(int(row.order_id) == int(order_id) for row in payment_db)
+    paid = False
+    if is_paid is True:
+        paid = True
 
-    is_paid = any(int(row.order_id) == int(id) for row in payment_db)
-    if status == 'release' and is_paid is False:
-        return jsonify({'order_id': id, 'status': False}), 403
+    if is_paid is False and status == 'release':
+        return jsonify(is_paid = False), 402
 
-    for row in order_db:
-        if int(row.order_id) == int(id):
-            if status == 'release' and is_paid:
-                row.status = status
-                return jsonify([row.__dict__]), 201
-            elif status == 'made':
-                row.status = status
-                return jsonify([row.__dict__]), 201
+    if row.status is not 'open' and status == 'open':
+        return jsonify(status=False), 404
 
-    return jsonify(order_id=False), 404
+    row.status = str(status)
+    return jsonify({
+        'data': row.__dict__,
+        'is_paid': paid
+    }) , 202
 
-
-@app.route("/payments", methods=['POST'])
-def add_payment():
+@app.route("/payments/<order_id>", methods=['POST'])
+def create_payment(order_id):
     parser = reqparse.RequestParser()
-    parser.add_argument('order_id', type=int)
-    parser.add_argument('payment_type', type=str)
+    parser.add_argument('payment_type', type=str,required=True)
+    parser.add_argument('card_name', type=str)
+    parser.add_argument('card_num', type=int)
+    parser.add_argument('card_exp', type=str)
     args = parser.parse_args()
 
-    id = int(args.get("order_id"))
-
-    if not any(row.order_id == id for row in order_db):
+    if not any(row.order_id == int(order_id) for row in order_db):
         return jsonify(order_id=False), 404
 
     payment_type = args.get("payment_type")
@@ -141,20 +161,37 @@ def add_payment():
         card_name = args.get("card_name")
         card_num = args.get("card_num")
         card_exp = args.get("card_exp")
-        payment_db.append((Payment(id, payment_type, card_name, card_num, card_exp)))
+        new = Payment(order_id, payment_type, card_name, card_num, card_exp)
+
     else:
-        payment_db.append((Payment(id, payment_type)))
+        new = Payment(order_id, payment_type)
+    payment_db.append(new)
+    order = next((x for x in order_db if x.order_id == int(order_id)), None)
+    return jsonify({
+        'data':[new.__dict__],
+        'order':[order.__dict__]
+    }), 201
 
-    return jsonify(order_id=id), 201
 
+@app.route("/payments/<order_id>", methods=['GET'])
+def get_payment(order_id):
+    order = next((x for x in order_db if x.order_id == int(order_id)), None)
+    payment = next((x for x in payment_db if x.order_id == int(order_id)), None)
 
-@app.route("/payments/<id>", methods=['GET'])
-def get_payment(id):
-    for row in payment_db:
-        if int(row.order_id) == int(id):
-            return jsonify(row.__dict__)
-    return jsonify(order_id=False), 404
+    if order is None:
+        jsonify(order_id=False), 404
+    elif payment is None:
+        return jsonify({
+            'payment': False,
+            'order': order.__dict__,
+        })
+    else:
+        return jsonify({
+            'payment': payment.__dict__,
+            'order': order.__dict__,
+        })
+
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
--- model.py
+++ model.py
@@ -1,16 +1,20 @@
 class Coffee:
     def __init__(self,coffee_id,coffee_name,cost):
         self.coffee_id = coffee_id
-        self.coffee_type = coffee_name
+        self.coffee_name = coffee_name
         self.cost = cost
 
 class Order:
-    def __init__(self, order_id, coffee_type, cost, status,additions):
+    def __init__(self, order_id, coffee_id,coffee_name, cost, status,additions):
         self.order_id = order_id
-        self.coffee_type = coffee_type
+        self.coffee_id = coffee_id
+        self.coffee_name = coffee_name
         self.cost = cost
         self.status = status
         self.additions = additions
+
+
+
 
 class Payment:
     def __init__(self, order_id, payment_type, card_name = None, card_number = None, card_exp = None):
